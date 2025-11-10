dc95e9d8054b1deacd81853466d6dfdd1c553d9f
Samata George <110812655+georgeeee3@users.noreply.github.com>
2025-11-10 15:33:58 +0200

    Add files via upload

---
+++ lamp-studenti/studenti/database.js
@@ -0,0 +1,43 @@
+// Acesta este fișierul tău database.js
+
+const products = {
+    "jojo1": {
+        title: "JoJo's Bizarre Adv. Part 7 Vol 1",
+        author: "Hirohiko Araki",
+        price: "120,00 lei",
+        format: "Paperback",
+        isbn: "9781974758371",
+        image: "https://nautilus.ro/gallery/pl/72/jojos-bizarre-adv-part-71-1.jpg",
+        description: "A multigenerational tale of the heroic Joestar family and their never-ending battle against evil!"
+    },
+    "jojo2": {
+        title: "JoJo's Bizarre Adv. Part 7 Vol 2",
+        author: "Hirohiko Araki",
+        price: "120,00 lei",
+        format: "Paperback",
+        isbn: "9781974758388",
+        image: "https://nautilus.ro/gallery/pl/72/jojos-bizarre-adv-part-72-1.jpg",
+        description: "The second volume of the critically acclaimed Steel Ball Run arc!"
+    },
+    "jojo3": {
+        title: "JoJo's Bizarre Adventure: Part 7-Steel Ball Run Vol. 3",
+        author: "Hirohiko Araki",
+        price: "105,60 lei",
+        rrp: "132,00 lei",
+        format: "Hardback",
+        isbn: "9781974758371",
+        image: "https://nautilus.ro/gallery/pl/72/jojos-bizarre-adv-part-73-1.jpg",
+        description: "A multigenerational tale of the heroic Joestar family and their never-ending battle against evil!",
+        publishDate: "Published 23.10.2025"
+    },
+    "pibble-palace": {
+        title: "Pibble Palace",
+        author: "Nume Autor",
+        price: "80,00 lei",
+        format: "Paperback",
+        isbn: "1234567890123",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM-KL2ViorLI6wKIMYsmAk_iLpUV3FthdQWA&s",
+        description: "O descriere pentru Pibble Palace."
+    }
+    // Adaugă restul cărților tale aici, cu un id unic...
+};
